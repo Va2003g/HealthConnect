ab4f4ed63bc31f7a53e9cd0cecde075096de0909
vanshgupta4661@gmail.com <vanshgupta4661@gmail.com>
2024-04-06 13:23:31 +0530

    Solving deployment issue

--- healthconnectfrontend/src/Pages/Registration/Login.jsx
+++ healthconnectfrontend/src/Pages/Registration/Login.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Template from '../../Components/Registration/Template'
-
+import { useSearchParams } from 'react-router-dom';
+import Reset_Password from './Reset_Password';
 const Login = () => {
+
+  const { resetToken } = useSearchParams();
+
   return (
-    <Template 
-      formType = "login"
-    />
-  )
+    resetToken ? <Reset_Password /> : <Template formType="login" />
+  );
+  // return (
+  //   {
+  //     resetToken ? (<Reset_Password/> ) : (<Template 
+  //       formType = "login"
+  //     />)
+  //   }
+  //   // <Template 
+  //   //   formType = "login"
+  //   // />
+  // )
 }
 
 export default Login;
